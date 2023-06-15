6b88f5dd435a63407a83075fc0174b7e8d434d86
Sofs16 <134558576+Sofs16@users.noreply.github.com>
2023-06-15 19:58:16 +0100

    Apresentação do Jogo e definir variáveis
    Foram efetuadas algumas alterações no boneco

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,9 +1,10 @@
-
+import os
+import random
 
 forca_torre = [
-    """
+"""
 _ _ __ _ _ _ _ _
-|	            |
+|               |
 |    	  _     |    _
 |    	 |_| _ _ _ _|_|
 |   	 |_ _ _ _ _ _ |
@@ -11,16 +12,16 @@ _ _ __ _ _ _ _ _
 |   	_|_ _ _ _ _ _ |_
 |      |_ _ _ _ _ _ _ _ |
 |      |_ _ _ _ _ _ _ _ |
-|  	   |_ _ _ _ _ _ _ _ |
+|      |_ _ _ _ _ _ _ _ |
 |     _|_ _ _ _ _ _ _ _ |_
 |     |_ _ _ _ _ _ _ _ _ _|
 |     |_ _ __ _ _ _ _ _ _ |
 |___________________________
 
 """,
- """
+"""
 _ _ __ _ _ _ _ _
-|	            |
+|               |
 |    	  _     |    _
 |    	 |_| _ _ _ _|_|
 |   	 |_ _ _ _ _ _ |
@@ -28,16 +29,16 @@ _ _ __ _ _ _ _ _
 |   	_|_ _ _ _ _ _ |_
 |      |_ _ _ _ _ _ _ _ |
 |      |_ _ _ _ _ _ _ _ |
-|  	   |_ _ _ _ _ _ _ _ |
+|      |_ _ _ _ _ _ _ _ |
 |      |_ _ _ _ _ _ _ _ |
 |     
 |    
 |___________________________
 
 """,
- """
+"""
 _ _ __ _ _ _ _ _
-|	            |
+|               |
 |    	  _     |    _
 |    	 |_| _ _ _ _|_|
 |   	 |_ _ _ _ _ _ |
@@ -52,9 +53,9 @@ _ _ __ _ _ _ _ _
 |___________________________
 
 """,
- """
+"""
 _ _ __ _ _ _ _ _
-|	            |
+|               |
 |    	  _     |    _
 |    	 |_| _ _ _ _|_|
 |   	 |_ _ _ _ _ _ |
@@ -69,10 +70,10 @@ _ _ __ _ _ _ _ _
 |___________________________
 
 """,
- """
+"""
 _ _ __ _ _ _ _ _
-|	            |
-|    	  _     |    _
+|               |
+|         _     |    _
 |    	 |_| _ _ _ _|_|
 |   	 |_ _ _ _ _ _ |
 |        
@@ -86,9 +87,9 @@ _ _ __ _ _ _ _ _
 |___________________________
 
 """,
- """
+"""
 _ _ __ _ _ _ _ _
-|	            |
+|               |
 |    	        |    
 |    	 
 |   	 
@@ -105,3 +106,24 @@ _ _ __ _ _ _ _ _
 """,
 ]
 
+tentativas = 5
+fase_forca = 6
+letras_utilizadas =[]
+palavra_secreta = ""
+continuar = False
+
+print()
+print("Os Craques do Código apresentam:")
+print()
+print(":::::: O Jogo da Forca - Versão Torre de Belém ::::::")
+
+print(forca_torre[5])
+
+print("Escolha o modo de jogo:")
+print("1. Palavra secreta escolhida aleatóriamente de uma lista")
+print("2. Palavra secreta à sua escolha")
+print()
+
+lista_palavras = ["bitaite", "relogio", "garrafa", "esternocleidomastoideu","otorrinolaringologista", "pinguim", "python"]
+
+escolha = input("Introduza 1 ou 2 para escolher o modo de jogo")
