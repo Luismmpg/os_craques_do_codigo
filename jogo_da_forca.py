d49208dea87da7837cff5952a19934b1b38261a7
alvt84 <alvito.santos.1884@gmail.com>
2023-06-15 20:52:14 +0100

    definiçao de loops
    detectar letras repetidas
    detectar letras que nao estao na palavra secreta
    definir final de jogo

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -127,3 +127,49 @@ print()
 lista_palavras = ["bitaite", "relogio", "garrafa", "esternocleidomastoideu","otorrinolaringologista", "pinguim", "python"]
 
 escolha = input("Introduza 1 ou 2 para escolher o modo de jogo")
+
+while continuar == False:
+    if escolha == "1":
+        palavra_secreta = random.choice(lista_palavras)
+        continuar = True 
+        os.system("cls")
+        print("optou por jogar com uma palavra aleatoria")
+        print()
+    elif escolha =="2":
+        os.system("cls")
+        print("optou por jogar com uma palavra escolhida por si")
+        print()
+        palavra_secreta = input ("escreva uma palavra a ser adivinhada: ").lower()
+        continuar= True
+        os.system("cls")
+    else:
+        continuar = False
+        escolha = input ("esse numero nao e valido escolha 1 ou 2 ")
+
+while tentativas > 0:
+    letras_certas = 0
+    print()
+    adivinha_a_letra = input("adivinhe uma letra ").lower()
+    print()
+    if adivinha_a_letra in letras_utilizadas:
+        print("essa letra ja foi usada, tente de novo")
+        print("letras usadas",letras_utilizadas)
+        print()
+        continue
+    else:
+        letras_utilizadas.append(adivinha_a_letra)
+        if adivinha_a_letra not in palavra_secreta:
+            tentativas -=1
+            fase_forca -=1
+            print(forca_torre[fase_forca-1])
+            print("essa letra nao esta na palavra,restam ",tentativas,"tentativas")
+            print()
+            print("letras usadas",letras_utilizadas)
+        if tentativas ==0: 
+            print()
+            print("voce perdeu,esgotou as 5 tentativas")
+            print("a palavra secreta era:",palavra_secreta )
+            print()
+            break
+
+
